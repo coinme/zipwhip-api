793aaa9d05febc576282ebcbc15fbffc39dc4cba
Ali Serghini <dev.serghini@gmail.com>
2014-04-04 15:52:20 -0700

    Design #15307: zipwhip-api DeliveredMessageTypeAdapter not parsing the signal message correctly

--- src/main/java/com/zipwhip/api/signals/dto/json/SignalProviderGsonBuilder.java
+++ src/main/java/com/zipwhip/api/signals/dto/json/SignalProviderGsonBuilder.java
@@ -6,6 +6,7 @@ import com.zipwhip.api.signals.dto.DeliveredMessage;
 import com.zipwhip.api.signals.dto.SubscribeCompleteContent;
 import com.zipwhip.signals2.address.Address;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.Date;
 
@@ -34,10 +35,27 @@ public class SignalProviderGsonBuilder {
                 }
             })
 //            .registerTypeHierarchyAdapter(Presence.class, new PresenceTypeAdapter())
+            .setFieldNamingStrategy(new CustomFieldNamingStrategy())
             .create();
 
     public static Gson getInstance() {
         return gson;
+    }
+
+    // This is needed to accommodate the legacy naming
+    private static class CustomFieldNamingStrategy implements FieldNamingStrategy {
+
+        @Override
+        public String translateName(Field f) {
+            final String fieldName = f.getName();
+
+            // SignalMessage has property hasAttachments. Json received has attachments
+            if ("hasAttachments".equals(fieldName)) {
+                return "attachments";
+            }
+
+            return fieldName;
+        }
     }
 
 }
